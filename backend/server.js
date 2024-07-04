86b241c8919495dbc65c5719dd1e9b8972dc1eb7
Nagy-Tóth Bence <bence.nagy-toth@irob.uni-obuda.hu>
2024-07-04 21:01:52 +0200

    more clarification added, more files ignored with .dockerignore template

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ import usersRoutes from "./routes/users.js";
 import cors from "cors";
 const app = express();
 configDotenv();
-const BACKEND_PORT = process.env.NODE_DOCKER_PORT || 3000;
+const BACKEND_PORT = process.env.BACKEND_PORT || 3000;
 app.use(express.json());
 app.use(cors());
 app.use("/users", usersRoutes);
